033fcfa63d1a6b49804b2476a05dc4ace273e801
pussyDestroyer <j.aldosn05@gmail.com>
2024-01-21 20:43:57 -0600

    Actualizacion: HomePage independiente

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_git/pages/home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("culero"),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
